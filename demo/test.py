da59e0dddf12eb2053dbc58c88b1165fed9e2f2b
Hristo Raykov <hristocr@gmail.com>
2020-03-25 14:43:49 +0200

    sectors and industries extracted

--- demo/test.py
+++ demo/test.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
 from stock_symbols.retrieve.debt_securities.dataset import retrieve_debt_sec_dataset
 
 debt_sec = retrieve_debt_sec_dataset()
+
+debt_sec = debt_sec[["symbol", "coupon_rate", "company_name", "sector", "industry", "month_3_aver_vol"]]
+
+sectors = pd.Series(debt_sec["sector"].unique())
+industries = pd.Series(debt_sec["industry"].unique())
